aff185bf92e4f0f00ee744d0d4688c67b588cd63
Freddy Henderson <hendersonsarah12@gmail.com>
2022-10-13 20:28:58 +0200

    adding new reviews now works

---
+++ db/migrate/20221013181513_change_column_type_of_rating.rb
@@ -0,0 +1,5 @@
+class ChangeColumnTypeOfRating < ActiveRecord::Migration[7.0]
+  def change
+    change_column(:reviews, :rating, :string)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_11_181834) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_13_181729) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
